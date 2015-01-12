79991feacbb504c2227526ba84722e96e6e93da1
Swidjit <alex@swidjit.com>
2015-01-12 12:15:23 -0500

    adjustments to finish signup / choose username

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
 
   def home
+    # Ensure we don't go into an infinite loop
+    return if action_name == 'finish_signup' || action_name == 'sign_in' || action_name == 'sign_up'
 
+    # Redirect to the 'finish_signup' page if the user
+    # email hasn't been verified yet
+    if current_user
+      if current_user.username.include?('dummy-')
+        redirect_to finish_signup_path(current_user)
+      end
+    end
   end
 
   def index
